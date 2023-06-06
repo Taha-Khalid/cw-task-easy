25f8521c21ad09786fcc057e4829d4f2db61ff11
Taha-Khalid <tahakhalid0320@gmail.com>
2023-06-06 08:13:02 +0500

    Uploaded the code for copying file from local backups to other public_html directory

---
+++ copy_backup_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+directory="/home/master/applications"
+
+#find "$directory" -maxdepth 1 -type d ! -path "$directory" -exec basename {} \; 
+cd $directory
+
+echo "Please enter the name you want to name the backup file in the destination folder"
+read dest_name
+
+for A in $(ls | awk '{print $NF}') 
+do 
+	echo $A 
+       	cp $directory/$A/local_backups/backup.tgz $directory/$A/public_html/$dest_name
+done
